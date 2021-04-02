cedf58105fce1abb2298235020fb0fdeec1b6ae5
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-02 15:56:15 -0700

    refactor html code

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,8 @@
 const Employee = require ('../lib/Employee');
+
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+  });
+  
+  
