6dd9c6ccae877c0687ab0b8515b901c7052ae421
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-03 21:53:48 -0700

    add team manager's questions

--- index.js
+++ index.js
@@ -4,147 +4,76 @@ const ManagerInfo = require("./lib/Manager");
 const EngineerInfo = require("./lib/Engineer");
 const InternInfo = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
+const path = require("path");
+const employ = []
+const HTMLOUTPUT_DIR = path.resolve(__dirname, "htmloutput");
+const pathOutput = path.join(HTMLOUTPUT_DIR, "testoutput.html");
 
-
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your name!');
-          return false;
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+function manager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the team manager’s name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter Employee ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter email address.",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Enter office number.",
+            name: "office"
         }
-      }
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: githubInput => {
-        if (githubInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub username!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAbout',
-      message: 'Would you like to enter some information about yourself for an "About" section?',
-      default: true
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:',
-      when: ({ confirmAbout }) => confirmAbout
-    }
-  ]);
-};
-
-const promptProject = portfolioData => {
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-
-  // If there's no 'projects' array property, create one
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-  }
-  return inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project name!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did you create this project with? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
-      {
-        type: 'input',
-        name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)',
-        validate: linkInput => {
-          if (linkInput) {
-            return true;
-          } else {
-            console.log('You need to enter a project GitHub link!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'confirm',
-        name: 'feature',
-        message: 'Would you like to feature this project?',
-        default: false
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddProject',
-        message: 'Would you like to enter another project?',
-        default: false
-      }
     ])
-    .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
-      } else {
-        return portfolioData;
-      }
+    // add manager to an employee array
+    .then(function (response) {
+        console.log(response)
+        emp.push(new Manager(response.name, response.id, response.email, response.office))
+        buildTeam();
     });
-};
+}
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+function buildTeam() {
+    inquirer.prompt([
+        {
+            type: "checkbox",
+            message: "Would you like to add someone to your team?",
+            name: "team",
+            choices: ["Engineer", "Intern", "I'm finished building my team"] 
+        }
+    ])
+    // add team members
+    .then(function (response) {
+        if (response.team === "Engineer") {
+            console.log("build engineer")
+            // prompt the engineer question block
+            engineer();
+        } else if (response.team === "Intern") {
+            console.log("build intern")
+            // prompt the intern question block
+            intern();
+        } else {
+            // exit function, call render function and write team to html.
+            console.log("finished, writing team")
+            fs.writeFile(outputPath, render(emp), function (err) {
+                if (err) {
+                    return
+                } else
+                console.log("team written successfully")
+            })
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+}
+// Questions for manager about Engineer
