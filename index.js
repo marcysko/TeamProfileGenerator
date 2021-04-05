5394f3de1669bbc8f7fec51af9b9109a4a3a63a0
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-04-05 13:33:26 -0700

    add CSS properties

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
